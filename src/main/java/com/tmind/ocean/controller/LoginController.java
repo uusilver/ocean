4ad8d1341058b93d0591b3aa7824b9bd1f81c000
cindy-gu <benbencin@163.com>
2016-04-18 09:36:04 +0800

    添加Kaptcha验证码

--- src/main/java/com/tmind/ocean/controller/LoginController.java
+++ src/main/java/com/tmind/ocean/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.tmind.ocean.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.tmind.ocean.entity.AgentUser;
 import com.tmind.ocean.entity.UserEntity;
 import com.tmind.ocean.model.UserTo;
@@ -9,6 +10,7 @@ import com.tmind.ocean.util.Constants;
 import com.tmind.ocean.util.MailUtil;
 import com.tmind.ocean.util.UniqueKeyGenerator;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -108,88 +110,56 @@ public class LoginController {
         return result;
     }
 
-    @RequestMapping("/code")
-    public void getCode(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+    //生成Kaptcha验证码
+    @Autowired
+    private Producer captchaProducer = null;
+    @RequestMapping(value = "/code")
+    public ModelAndView getCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        HttpSession session = request.getSession();
+        String code = (String)session.getAttribute(com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY);
+        System.out.println("******************验证码是: " + code + "******************");
 
-        // 定义图像buffer
-        BufferedImage buffImg = new BufferedImage(width, height,
-                BufferedImage.TYPE_INT_RGB);
-//      Graphics2D gd = buffImg.createGraphics();
-        //Graphics2D gd = (Graphics2D) buffImg.getGraphics();
-        Graphics gd = buffImg.getGraphics();
-        // 创建一个随机数生成器类
-        Random random = new Random();
-        // 将图像填充为白色
-        gd.setColor(Color.WHITE);
-        gd.fillRect(0, 0, width, height);
+        response.setDateHeader("Expires", 0);
 
-        // 创建字体，字体的大小应该根据图片的高度来定。
-        Font font = new Font("Fixedsys", Font.BOLD, fontHeight);
-        // 设置字体。
-        gd.setFont(font);
+        // Set standard HTTP/1.1 no-cache headers.
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
 
-        // 画边框。
-        gd.setColor(Color.BLACK);
-        gd.drawRect(0, 0, width - 1, height - 1);
+        // Set IE extended HTTP/1.1 no-cache headers (use addHeader).
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
 
-        // 随机产生40条干扰线，使图象中的认证码不易被其它程序探测到。
-        gd.setColor(Color.BLACK);
-        for (int i = 0; i < 40; i++) {
-            int x = random.nextInt(width);
-            int y = random.nextInt(height);
-            int xl = random.nextInt(12);
-            int yl = random.nextInt(12);
-            gd.drawLine(x, y, x + xl, y + yl);
+        // Set standard HTTP/1.0 no-cache header.
+        response.setHeader("Pragma", "no-cache");
+
+        // return a jpeg
+        response.setContentType("image/jpeg");
+
+        // create the text for the image
+        String capText = captchaProducer.createText();
+
+        // store the text in the session
+        session.setAttribute(com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY, capText);
+
+        // create the image with the text
+        BufferedImage bi = captchaProducer.createImage(capText);
+        ServletOutputStream out = response.getOutputStream();
+
+        // write the data out
+        ImageIO.write(bi, "jpg", out);
+        try {
+            out.flush();
+        } finally {
+            out.close();
         }
-
-        // randomCode用于保存随机产生的验证码，以便用户登录后进行验证。
-        StringBuffer randomCode = new StringBuffer();
-        int red = 0, green = 0, blue = 0;
-
-        // 随机产生codeCount数字的验证码。
-        for (int i = 0; i < codeCount; i++) {
-            // 得到随机产生的验证码数字。
-            String code = String.valueOf(codeSequence[random.nextInt(36)]);
-            // 产生随机的颜色分量来构造颜色值，这样输出的每位数字的颜色值都将不同。
-            red = random.nextInt(255);
-            green = random.nextInt(255);
-            blue = random.nextInt(255);
-
-            // 用随机产生的颜色将验证码绘制到图像中。
-            gd.setColor(new Color(red, green, blue));
-            gd.drawString(code, (i + 1) * xx, codeY);
-
-            // 将产生的四个随机数组合在一起。
-            randomCode.append(code);
-        }
-        // 将四位数字的验证码保存到Session中。
-        HttpSession session = req.getSession();
-        System.out.print(randomCode);
-        session.setAttribute("code", randomCode.toString());
-
-        // 禁止图像缓存。
-        resp.setHeader("Pragma", "no-cache");
-        resp.setHeader("Cache-Control", "no-cache");
-        resp.setDateHeader("Expires", 0);
-
-        resp.setContentType("image/jpeg");
-
-        // 将图像输出到Servlet输出流中。
-        ServletOutputStream sos = resp.getOutputStream();
-        ImageIO.write(buffImg, "jpeg", sos);
-        sos.close();
-
+        return null;
     }
 
+    //验证码校验
     @RequestMapping(value = "validateJCode", method = RequestMethod.POST)
-    public
-    @ResponseBody
-    String validateJCode(@RequestParam String jCode, HttpServletRequest request) {
-        String jCodeInSession = (String) request.getSession().getAttribute("code");
-        if (jCodeInSession != null && jCodeInSession.equals(jCode)) {
+    public @ResponseBody String validateJCode(@RequestParam String jCode, HttpServletRequest request){
+        String jCodeInSession = (String) request.getSession().getAttribute(com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY);
+        if(jCodeInSession!=null&&jCodeInSession.equals(jCode)){
             return "success";
-        } else {
+        }else{
             return "failed";
         }
     }
