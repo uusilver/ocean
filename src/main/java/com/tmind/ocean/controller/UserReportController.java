c30df97dc7402631bcd8f6a4f2b3c65291c0bc01
vani_junying@hotmail.com <vani_junying@hotmail.com>
2016-09-22 21:55:40 +0800

    用户扫码统计从数据库读表

--- src/main/java/com/tmind/ocean/controller/UserReportController.java
+++ src/main/java/com/tmind/ocean/controller/UserReportController.java
@@ -2,6 +2,8 @@ package com.tmind.ocean.controller;
 
 import com.google.gson.Gson;
 import com.tmind.ocean.model.UserReportModel;
+import com.tmind.ocean.util.HibernateUtil;
+import org.hibernate.SQLQuery;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by lijunying on 16/9/18.
  */
+@SuppressWarnings("ALL")
 @Controller
 @RequestMapping("report")
 public class UserReportController {
@@ -29,26 +33,64 @@ public class UserReportController {
         String[] last6Months = getLast6Months();
         StringBuilder yearMonthBuilder = new StringBuilder();
         yearMonthBuilder.append("[");
+
+        //正常扫码数
+        StringBuilder normalScanBuilder = new StringBuilder();
+        normalScanBuilder.append("[");
+
+        //异常扫码数
+        StringBuilder warningScanBuilder = new StringBuilder();
+        warningScanBuilder.append("[");
+
+        //平均数
+        StringBuilder averageScanBuilder = new StringBuilder();
+        averageScanBuilder.append("[");
+
+        int pieNormalScan = 0;
+        int pieWarningScan = 0;
+
         int index = 0;
         for(String s : last6Months){
-            if(index<5)
+            //获取sql
+            SQLQuery query = HibernateUtil.getSessionFactory().openSession().createSQLQuery("select normal_scan_times, warning_scan_times, average from user_statistics where user_id = ? and year_month_str = ?");
+            query.setParameter(0, userId);
+            query.setParameter(1, s);
+            List<Object[]> list = query.list();
+            if(list.size()==0){
+                normalScanBuilder.append("0").append(",");
+                warningScanBuilder.append("0").append(",");
+                averageScanBuilder.append("0").append(",");
+            }
+            else{
+                Object[] objects = list.get(0);
+                normalScanBuilder.append(objects[0]).append(",");
+                warningScanBuilder.append(objects[1]).append(",");
+                averageScanBuilder.append(objects[2]).append(",");
+                pieNormalScan+=Integer.valueOf(String.valueOf(objects[0]));
+                pieWarningScan+=Integer.valueOf(String.valueOf(objects[1]));
+
+
+            }
+
                 yearMonthBuilder.append("'").append(s).append("'").append(",");
-            else
-                yearMonthBuilder.append("'").append(s).append("'");
+
             index++;
         }
-            yearMonthBuilder.append("]");
+        yearMonthBuilder.append("]");
+        normalScanBuilder.append("]");
+        warningScanBuilder.append("]");
+        averageScanBuilder.append("]");
         userReportModel.setMonth(yearMonthBuilder.toString());
 
         //抬头
         userReportModel.setTitle(last6Months[0]+"月～"+last6Months[5]+"月扫码分析统计");
 
-        //正常扫码数
+        userReportModel.setPieChartNormalScan(pieNormalScan);
+        userReportModel.setPieChartWarningScan(pieWarningScan);
 
-        //异常扫码数
-
-        //平均数
-
+        userReportModel.setNormalScan(normalScanBuilder.toString());
+        userReportModel.setWarningScan(warningScanBuilder.toString());
+        userReportModel.setAverageScan(averageScanBuilder.toString());
 
         Gson gson = new Gson();
         return gson.toJson(userReportModel);
--- src/main/java/com/tmind/ocean/model/UserReportModel.java
+++ src/main/java/com/tmind/ocean/model/UserReportModel.java
@@ -7,6 +7,11 @@ public class UserReportModel {
 
     private String month;
     private String title;
+    private String normalScan;
+    private String warningScan;
+    private String averageScan;
+    private int pieChartNormalScan;
+    private int pieChartWarningScan;
 
     public String getMonth() {
         return month;
@@ -22,5 +27,45 @@ public class UserReportModel {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getNormalScan() {
+        return normalScan;
+    }
+
+    public void setNormalScan(String normalScan) {
+        this.normalScan = normalScan;
+    }
+
+    public String getWarningScan() {
+        return warningScan;
+    }
+
+    public void setWarningScan(String warningScan) {
+        this.warningScan = warningScan;
+    }
+
+    public String getAverageScan() {
+        return averageScan;
+    }
+
+    public void setAverageScan(String averageScan) {
+        this.averageScan = averageScan;
+    }
+
+    public int getPieChartNormalScan() {
+        return pieChartNormalScan;
+    }
+
+    public void setPieChartNormalScan(int pieChartNormalScan) {
+        this.pieChartNormalScan = pieChartNormalScan;
+    }
+
+    public int getPieChartWarningScan() {
+        return pieChartWarningScan;
+    }
+
+    public void setPieChartWarningScan(int pieChartWarningScan) {
+        this.pieChartWarningScan = pieChartWarningScan;
     }
 }
